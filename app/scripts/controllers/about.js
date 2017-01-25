db503ef19d010080a7683f6a872a6beb72665c47
mesalmi <msalmizied@gmail.com>
2017-01-25 16:54:56 +0100

    first_commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name monAgendaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the monAgendaApp
+ */
+angular.module('monAgendaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name monAgendaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the monAgendaApp
+ */
+angular.module('monAgendaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
